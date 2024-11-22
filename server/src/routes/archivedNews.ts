aa615886b0a72d723eac567cb1550e747e3febc8
Milagros Niro <niromilagros@gmail.com>
2024-11-22 13:24:38 +0100

    delete commented code & console.log

--- server/src/routes/archivedNews.ts
+++ server/src/routes/archivedNews.ts
@@ -4,11 +4,3 @@ import { ArchivedNewsController } from '../controllers/archivedNews';
 export const archivedNewsRoutes = Router()
 
 archivedNewsRoutes.get('/', ArchivedNewsController.getAll)
-// archivedNewsRoutes.put('/:id',
-//     param('id').isMongoId().withMessage('Not valid ID'),
-//     handleInputErrors,
-//      ArchivedNewsController.updateNew)
-// archivedNewsRoutes.delete('/:id', 
-//     param('id').isMongoId().withMessage('Not valid ID'),
-//     handleInputErrors,
-//     ArchivedNewsController.deleteNew)
